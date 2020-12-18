58d92682a28abb4b7e88cbe536aa0c1ed2d628a3
Shun Nakano <nakano_syun@waku-2.com>
2020-12-18 18:01:29 +0900

    feat: SignUpコンポーネントの実装

---
+++ client/src/components/SignUp/SignUp.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import { AuthForm } from '../AuthForm';
+
+export const SignUp: React.VFC = () => <AuthForm type="signup" />;
---
+++ client/src/components/SignUp/index.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import { SignUp as Presentational } from './SignUp';
+
+export const SignUp: React.VFC = () => <Presentational />;

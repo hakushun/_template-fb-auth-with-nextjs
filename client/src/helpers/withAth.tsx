a9227a29aab6c775a7110b44e1429408e42bbc6a
Shun Nakano <shun.nakano@nijibox.co.jp>
2020-12-19 18:05:30 +0900

    feat: withAuthにuserによる条件分岐実装

--- client/src/helpers/withAth.tsx
+++ client/src/helpers/withAth.tsx
@@ -1,42 +1,40 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useRouter } from 'next/router';
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import initFirebase from '../libs/auth/initFirebase';
 import { removeUserCookie, setUserCookie } from '../libs/auth/userCookies';
 import { mapUserData } from '../libs/auth/mapUserData';
-import { authUser } from '../redux/modules/user';
+import { authUser, selectUser } from '../redux/modules/user';
+import { PageLoader } from '../components/PageLoader';
 
 initFirebase();
 
-// TODO: Loading の追加
 export const withAuth = (Component: React.FC): React.FC => (
   props: any,
 ): JSX.Element => {
   const dispatch = useDispatch();
   const router = useRouter();
+  const user = useSelector(selectUser);
 
   useEffect(() => {
-    const cancelAuthListener = firebase
-      .auth()
-      .onIdTokenChanged(async (user) => {
-        if (user) {
-          const userData = await mapUserData(user);
-          setUserCookie(userData);
-          dispatch(authUser(userData));
-          router.push('/mypage');
-        } else {
-          removeUserCookie();
-          dispatch(authUser(null));
-          router.push('/');
-        }
-      });
+    const cancelAuthListener = firebase.auth().onIdTokenChanged(async (usr) => {
+      if (usr) {
+        const userData = await mapUserData(usr);
+        setUserCookie(userData);
+        dispatch(authUser(userData));
+      } else {
+        removeUserCookie();
+        dispatch(authUser(null));
+        router.push('/');
+      }
+    });
     return () => {
       cancelAuthListener();
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  return <Component {...props} />;
+  return <>{!user ? <PageLoader /> : <Component {...props} />}</>;
 };

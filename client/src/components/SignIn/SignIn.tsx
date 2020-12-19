14d65b759e7b1d56eeb1ed55b96ab62142e8a7c3
Shun Nakano <shun.nakano@nijibox.co.jp>
2020-12-19 14:04:03 +0900

    feat: SignInにuseAuthを実装

--- client/src/components/SignIn/SignIn.tsx
+++ client/src/components/SignIn/SignIn.tsx
@@ -3,9 +3,13 @@ import React from 'react';
 import { AuthForm } from '../AuthForm';
 import styles from './index.module.scss';
 
-export const SignIn: React.VFC = () => (
+type Props = {
+  signin: (_value: { email: string; password: string }) => Promise<void>;
+};
+
+export const SignIn: React.VFC<Props> = ({ signin }) => (
   <>
-    <AuthForm type="signin" />
+    <AuthForm type="signin" onSubmit={signin} />
     <div className={styles.root}>
       If you do not have an account, please{' '}
       <Link href="signup">
--- client/src/components/SignIn/index.tsx
+++ client/src/components/SignIn/index.tsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import { useAuth } from '../../libs/auth/useAuth';
 import { SignIn as Presentational } from './SignIn';
 
-export const SignIn: React.VFC = () => <Presentational />;
+export const SignIn: React.VFC = () => {
+  const { signin } = useAuth();
+  return <Presentational signin={signin} />;
+};

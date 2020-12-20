a01dd9d47b83ffec8233a9a0c1f5a3c3d53311e9
Shun Nakano <shun.nakano@nijibox.co.jp>
2020-12-20 18:04:24 +0900

    feat: modules/menuの実装

--- client/src/redux/modules/reducers.ts
+++ client/src/redux/modules/reducers.ts
@@ -1,10 +1,11 @@
 import { combineReducers } from 'redux';
 import user from './user';
 import dialog from './dialog';
+import menu from './menu';
 
 const rootReducer = combineReducers({
   resources: combineReducers({ user }),
-  ui: combineReducers({ dialog }),
+  ui: combineReducers({ dialog, menu }),
 });
 
 export type RootState = ReturnType<typeof rootReducer>;

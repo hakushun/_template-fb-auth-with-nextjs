322f99c6e174161319d020ffd878216661ceb020
Shun Nakano <nakano_syun@waku-2.com>
2020-12-21 20:02:38 +0900

    refactor: ProjectコンポーネントでTaskListとActivityList の利用

--- client/src/components/Project/Project.tsx
+++ client/src/components/Project/Project.tsx
@@ -1,86 +1,58 @@
-import Link from 'next/link';
 import React from 'react';
+import { ActivityList } from '../ActivityList';
+import { TaskList } from '../TaskList';
 import styles from './index.module.scss';
 
 export const Project: React.VFC = () => (
   <section className={styles.root}>
     <div className={styles.heading}>
       <h2 className={styles.title}>プロジェクト名</h2>
-      <button type="button" className={styles.button}>
-        編集
-      </button>
     </div>
-    <div className={styles.project}>
-      <h3 className={styles.subtitle}>プロジェクト詳細</h3>
-      <div className={styles.projectWrapper}>
-        <dl>
-          <dt>概要</dt>
-          <dd>概要文</dd>
+    <div className={styles.wrapper}>
+      <div className={styles.subheading}>
+        <h3 className={styles.subtitle}>プロジェクト詳細</h3>
+        <button type="button" className={styles.button}>
+          編集
+        </button>
+      </div>
+      <div className={styles.inner}>
+        <dl className={styles.projectItem}>
+          <dt className={styles.projectLabel}>概要</dt>
+          <dd className={styles.projectDescription}>
+            アイウエオアイウエオアイウエオアイウエオアイウエオアイウエオアイウエオアイウエオアイウエオアイウエオ
+          </dd>
         </dl>
-        <dl>
-          <dt>期限</dt>
-          <dd>2020/12/12</dd>
+        <dl className={styles.projectItem}>
+          <dt className={styles.projectLabel}>期限</dt>
+          <dd className={styles.projectDescription}>2020/12/12</dd>
         </dl>
-        <dl>
-          <dt>登録日</dt>
-          <dd>2020/12/12</dd>
+        <dl className={styles.projectItem}>
+          <dt className={styles.projectLabel}>登録日</dt>
+          <dd className={styles.projectDescription}>2020/12/12</dd>
         </dl>
-        <dl>
-          <dt>最終更新日</dt>
-          <dd>2020/12/12</dd>
+        <dl className={styles.projectItem}>
+          <dt className={styles.projectLabel}>最終更新日</dt>
+          <dd className={styles.projectDescription}>2020/12/12</dd>
         </dl>
       </div>
     </div>
-    <div className={styles.task}>
-      <div className={styles.heading}>
+    <div className={styles.wrapper}>
+      <div className={styles.subheading}>
         <h3 className={styles.subtitle}>タスク一覧</h3>
         <button type="button" className={styles.button}>
           追加
         </button>
       </div>
-      <div className={styles.taskWrapper}>
-        <ul className={styles.taskList}>
-          <li className={styles.taskItem}>
-            <Link href="/tasks/1">
-              <a id="task_1" className={styles.taskLink}>
-                <div className={styles.taskStatus}>進行中</div>
-                <div className={styles.taskName}>タスク名</div>
-                <div className={styles.taskDuedate}>
-                  期限:<span className={styles.taskDate}>2020/12/12</span>
-                </div>
-              </a>
-            </Link>
-          </li>
-        </ul>
-      </div>
+      <TaskList />
     </div>
-    <div className={styles.activity}>
-      <div className={styles.heading}>
+    <div className={styles.wrapper}>
+      <div className={styles.subheading}>
         <h3 className={styles.subtitle}>アクティビティ</h3>
         <button type="button" className={styles.button}>
           追加
         </button>
       </div>
-      <div className={styles.activityWrapper}>
-        <ul className={styles.activityList}>
-          <li className={styles.activityItem}>
-            <div className={styles.activityInner}>
-              <span className={styles.activityDate}>2020/12/12</span>
-            </div>
-            <div className={styles.activityInner}>
-              <p className={styles.activityComment}>コメント欄</p>
-            </div>
-          </li>
-          <li>
-            <div className={styles.activityInner}>
-              <span className={styles.activityDate}>2020/12/12</span>
-            </div>
-            <div className={styles.activityInner}>
-              <p className={styles.activityComment}>コメント欄</p>
-            </div>
-          </li>
-        </ul>
-      </div>
+      <ActivityList />
     </div>
   </section>
 );

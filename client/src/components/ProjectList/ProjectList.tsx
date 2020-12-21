bd8f4f8d3f9da16ba41c588014e847add9672f9f
Shun Nakano <nakano_syun@waku-2.com>
2020-12-21 20:00:29 +0900

    refactor: ProjectListの整理

--- client/src/components/ProjectList/ProjectList.tsx
+++ client/src/components/ProjectList/ProjectList.tsx
@@ -4,44 +4,43 @@ import styles from './index.module.scss';
 
 // TODO 新規作成ボタン、ソート用のセレクトボックス、検索ボックスの実装
 export const ProjectList: React.VFC = () => (
-  <section className={styles.root}>
-    <h2 className={styles.title}>プロジェクト一覧</h2>
-    <div className={styles.wrapper}>
-      <ul className={styles.list}>
-        <li className={styles.item}>
-          <Link href="/projects/1">
-            <a id="project_1" className={styles.link}>
-              <div className={styles.status}>未着手</div>
-              <div className={styles.name}>プロジェクト名</div>
-              <div className={styles.task}>
-                残タスク:<span className={styles.number}>0</span>
-              </div>
-            </a>
-          </Link>
-        </li>
-        <li className={styles.item}>
-          <Link href="/projects/2">
-            <a id="project_2" className={styles.link}>
-              <div className={styles.status}>進行中</div>
-              <div className={styles.name}>プロジェクト名</div>
-              <div className={styles.task}>
-                残タスク:<span className={styles.number}>1000</span>
-              </div>
-            </a>
-          </Link>
-        </li>{' '}
-        <li className={styles.item}>
-          <Link href="/projects/3">
-            <a id="project_3" className={styles.link}>
-              <div className={styles.status}>完了</div>
-              <div className={styles.name}>プロジェクト名</div>
-              <div className={styles.task}>
-                残タスク:<span className={styles.number}>0</span>
-              </div>
-            </a>
-          </Link>
-        </li>
-      </ul>
-    </div>
-  </section>
+  <div className={styles.wrapper}>
+    <ul className={styles.list}>
+      <li className={styles.item}>
+        <Link href="/projects/1">
+          <a id="project_1" className={styles.link}>
+            <div className={styles.status}>未着手</div>
+            <div className={styles.name}>プロジェクト名</div>
+            <div className={styles.task}>
+              残タスク:<span className={styles.number}>0</span>
+            </div>
+          </a>
+        </Link>
+      </li>
+      <li className={styles.item}>
+        <Link href="/projects/2">
+          <a id="project_2" className={styles.link}>
+            <div className={styles.status}>進行中</div>
+            <div className={styles.name}>
+              プロジェクト名プロジェクト名プロジェクト名プロジェクト名プロジェクト名
+            </div>
+            <div className={styles.task}>
+              残タスク:<span className={styles.number}>1000</span>
+            </div>
+          </a>
+        </Link>
+      </li>{' '}
+      <li className={styles.item}>
+        <Link href="/projects/3">
+          <a id="project_3" className={styles.link}>
+            <div className={styles.status}>完了</div>
+            <div className={styles.name}>プロジェクト名</div>
+            <div className={styles.task}>
+              残タスク:<span className={styles.number}>0</span>
+            </div>
+          </a>
+        </Link>
+      </li>
+    </ul>
+  </div>
 );

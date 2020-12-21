2f7dd49508fe48ba3e5009438ba5969e3c66a8ea
Shun Nakano <nakano_syun@waku-2.com>
2020-12-21 19:58:15 +0900

    feat: BargerMenuの修正

--- client/src/components/BargerMenu/index.tsx
+++ client/src/components/BargerMenu/index.tsx
@@ -1,7 +1,6 @@
 import Link from 'next/link';
 import React from 'react';
 import clsx from 'clsx';
-import { Overlay } from '../Overlay';
 import styles from './index.module.scss';
 import { navList } from '../../config/navList';
 
@@ -15,27 +14,28 @@ export const BargerMenu: React.VFC<Props> = ({
   toggleMenu,
   logout,
 }) => (
-  <Overlay>
-    <nav className={styles.nav}>
-      <ul className={clsx(styles.navList, isOpened && styles.isOpened)}>
-        {navList.map((item) => (
-          <li className={styles.navItem} key={item.id}>
-            <Link href={item.href}>
-              <button className={styles.navLink} onClick={() => toggleMenu()}>
-                {item.label}
-              </button>
-            </Link>
-          </li>
-        ))}
-        <li className={styles.navItem}>
-          <button
-            type="button"
-            className={styles.navLink}
-            onClick={() => logout()}>
-            Logout
-          </button>
+  <nav
+    className={clsx(styles.nav, isOpened && styles.isOpened)}
+    aria-hidden={!isOpened}>
+    <ul className={styles.navList}>
+      {navList.map((item) => (
+        <li className={styles.navItem} key={item.id}>
+          <Link href={item.href}>
+            {/* eslint-disable jsx-a11y/click-events-have-key-events  */}
+            <a className={styles.navLink} onClick={() => toggleMenu()}>
+              {item.label}
+            </a>
+          </Link>
         </li>
-      </ul>
-    </nav>
-  </Overlay>
+      ))}
+      <li className={styles.navItem}>
+        <button
+          type="button"
+          className={styles.navLink}
+          onClick={() => logout()}>
+          Logout
+        </button>
+      </li>
+    </ul>
+  </nav>
 );

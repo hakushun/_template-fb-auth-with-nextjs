c26fb3b16dcde843ccea20bd1c3584f321b8e227
Shun Nakano <nakano_syun@waku-2.com>
2020-12-22 12:58:23 +0900

    refactor: TaskListコンポーネントにListHeader設置

--- client/src/components/TaskList/TaskList.tsx
+++ client/src/components/TaskList/TaskList.tsx
@@ -1,18 +1,18 @@
 import Link from 'next/link';
 import React from 'react';
+import { ListHeader } from '../ListHeader';
 import styles from './index.module.scss';
 
 export const TaskList: React.VFC = () => (
   <div className={styles.wrapper}>
+    <ListHeader context="task" />
     <ul className={styles.list}>
       <li className={styles.item}>
         <Link href="/tasks/1">
           <a id="task_1" className={styles.link}>
             <div className={styles.status}>進行中</div>
             <div className={styles.name}>タスク名</div>
-            <div className={styles.duedate}>
-              期限:<span className={styles.date}>2020/1/1</span>
-            </div>
+            <div className={styles.duedate}>2020/1/1</div>
           </a>
         </Link>
       </li>
@@ -23,9 +23,7 @@ export const TaskList: React.VFC = () => (
             <div className={styles.name}>
               アイウエオアイウエオアイウエオアイウエオアイウエオアイウエオアイウエオアイウエオアイウエオアイウエオ
             </div>
-            <div className={styles.duedate}>
-              期限:<span className={styles.date}>2020/12/12</span>
-            </div>
+            <div className={styles.duedate}>2020/12/12</div>
           </a>
         </Link>
       </li>
@@ -36,9 +34,7 @@ export const TaskList: React.VFC = () => (
             <div className={styles.name}>
               アイウエオアイウエオアイウエオアイウエオアイウエオアイウエオアイウエオアイウエオアイウエオアイウエオ
             </div>
-            <div className={styles.duedate}>
-              期限:<span className={styles.taskDate}>2020/12/12</span>
-            </div>
+            <div className={styles.duedate}>2020/12/12</div>
           </a>
         </Link>
       </li>

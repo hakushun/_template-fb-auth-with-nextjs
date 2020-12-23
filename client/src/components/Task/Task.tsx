6f48cdbf147397c750d13a3da5b65d361b727ee1
Shun Nakano <nakano_syun@waku-2.com>
2020-12-23 20:02:43 +0900

    feat: reduxと接続

--- client/src/components/Task/Task.tsx
+++ client/src/components/Task/Task.tsx
@@ -1,68 +1,105 @@
 import Link from 'next/link';
 import React from 'react';
+import clsx from 'clsx';
 import { ActivityList } from '../ActivityList';
 import { TaskList } from '../TaskList';
 import styles from './index.module.scss';
+import { TaskStatusList } from '../TaskStatusList';
+import { TaskForm } from '../TaskForm';
+import { ActivityForm } from '../ActivityForm';
 
-export const Task: React.VFC = () => (
-  <section className={styles.root}>
-    <div className={styles.heading}>
-      <h2 className={styles.title}>タスク名</h2>
-      <span className={styles.status}>進行中</span>
-    </div>
-    <div className={styles.linkWrapper}>
-      <Link href="/projects/1">
-        <a className={styles.link}>[プロジェクト名]への導線</a>
-      </Link>
-    </div>
-    <div className={styles.wrapper}>
-      <div className={styles.subheading}>
-        <h3 className={styles.subtitle}>タスク詳細</h3>
-        <button type="button" className={styles.action}>
-          <img src="/images/icon-edit.svg" alt="タスクを編集する" />
+type Props = {
+  isOpened: boolean;
+  toggleList: () => void;
+  toggleTaskModal: () => void;
+  toggleActivityModal: () => void;
+};
+export const Task: React.VFC<Props> = ({
+  isOpened,
+  toggleList,
+  toggleTaskModal,
+  toggleActivityModal,
+}) => (
+  <>
+    <TaskForm />
+    <ActivityForm />
+    <section className={styles.root}>
+      <div className={styles.heading}>
+        <h2 className={styles.title}>タスク名</h2>
+        <button className={styles.status} onClick={() => toggleList()}>
+          進行中
         </button>
+        <div
+          className={clsx(
+            styles.statusListWrapper,
+            isOpened && styles.isOpened,
+          )}>
+          <TaskStatusList />
+        </div>
       </div>
-      <div className={styles.inner}>
-        <dl className={styles.item}>
-          <dt className={styles.label}>概要</dt>
-          <dd className={styles.description}>
-            アイウエオアイウエオアイウエオアイウエオアイウエオアイウエオアイウエオアイウエオアイウエオアイウエオ
-          </dd>
-        </dl>
-        <dl className={styles.item}>
-          <dt className={styles.label}>期限</dt>
-          <dd className={styles.description}>2020/12/12</dd>
-        </dl>
-        <dl className={styles.item}>
-          <dt className={styles.label}>登録日</dt>
-          <dd className={styles.description}>2020/12/12</dd>
-        </dl>
-        <dl className={styles.item}>
-          <dt className={styles.label}>最終更新日</dt>
-          <dd className={styles.description}>2020/12/12</dd>
-        </dl>
+      <div className={styles.linkWrapper}>
+        <Link href="/projects/1">
+          <a className={styles.link}>[プロジェクト名]への導線</a>
+        </Link>
       </div>
-    </div>
-    <div className={styles.wrapper}>
-      <div className={styles.subheading}>
-        <h3 className={styles.subtitle}>[プロジェクト名]のタスク一覧</h3>
-        <button type="button" className={styles.action}>
-          <img src="/images/icon-circle-plus.svg" alt="タスクを追加する" />
-        </button>
+      <div className={styles.wrapper}>
+        <div className={styles.subheading}>
+          <h3 className={styles.subtitle}>タスク詳細</h3>
+          <button
+            type="button"
+            className={styles.action}
+            onClick={() => toggleTaskModal()}>
+            <img src="/images/icon-edit.svg" alt="タスクを編集する" />
+          </button>
+        </div>
+        <div className={styles.inner}>
+          <dl className={styles.item}>
+            <dt className={styles.label}>概要</dt>
+            <dd className={styles.description}>
+              アイウエオアイウエオアイウエオアイウエオアイウエオアイウエオアイウエオアイウエオアイウエオアイウエオ
+            </dd>
+          </dl>
+          <dl className={styles.item}>
+            <dt className={styles.label}>期限</dt>
+            <dd className={styles.description}>2020/12/12</dd>
+          </dl>
+          <dl className={styles.item}>
+            <dt className={styles.label}>登録日</dt>
+            <dd className={styles.description}>2020/12/12</dd>
+          </dl>
+          <dl className={styles.item}>
+            <dt className={styles.label}>最終更新日</dt>
+            <dd className={styles.description}>2020/12/12</dd>
+          </dl>
+        </div>
       </div>
-      <TaskList />
-    </div>
-    <div className={styles.wrapper}>
-      <div className={styles.subheading}>
-        <h3 className={styles.subtitle}>アクティビティ</h3>
-        <button type="button" className={styles.action}>
-          <img
-            src="/images/icon-circle-plus.svg"
-            alt="アクティビティを追加する"
-          />
-        </button>
+      <div className={styles.wrapper}>
+        <div className={styles.subheading}>
+          <h3 className={styles.subtitle}>[プロジェクト名]のタスク一覧</h3>
+          <button
+            type="button"
+            className={styles.action}
+            onClick={() => toggleTaskModal()}>
+            <img src="/images/icon-circle-plus.svg" alt="タスクを追加する" />
+          </button>
+        </div>
+        <TaskList />
       </div>
-      <ActivityList />
-    </div>
-  </section>
+      <div className={styles.wrapper}>
+        <div className={styles.subheading}>
+          <h3 className={styles.subtitle}>アクティビティ</h3>
+          <button
+            type="button"
+            className={styles.action}
+            onClick={() => toggleActivityModal()}>
+            <img
+              src="/images/icon-circle-plus.svg"
+              alt="アクティビティを追加する"
+            />
+          </button>
+        </div>
+        <ActivityList />
+      </div>
+    </section>
+  </>
 );
--- client/src/components/Task/index.tsx
+++ client/src/components/Task/index.tsx
@@ -1,7 +1,34 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { withAuth } from '../../helpers/withAuth';
+import {
+  selectStatusList,
+  toggleStatusList,
+} from '../../redux/modules/dropdown';
+import { toggleActivityForm, toggleTaskForm } from '../../redux/modules/modal';
 import { Task as Presentational } from './Task';
 
-const Component: React.VFC = () => <Presentational />;
+const Component: React.VFC = () => {
+  const dispatch = useDispatch();
+  const isOpened = useSelector(selectStatusList);
+
+  const toggleList = () => {
+    dispatch(toggleStatusList());
+  };
+  const toggleTaskModal = () => {
+    dispatch(toggleTaskForm());
+  };
+  const toggleActivityModal = () => {
+    dispatch(toggleActivityForm());
+  };
+  return (
+    <Presentational
+      isOpened={isOpened}
+      toggleList={toggleList}
+      toggleTaskModal={toggleTaskModal}
+      toggleActivityModal={toggleActivityModal}
+    />
+  );
+};
 
 export const Task = withAuth(Component);

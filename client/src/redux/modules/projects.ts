e918929346fd171066d820dcde0a5dc8c0c6c4a1
Shun Nakano <nakano_syun@waku-2.com>
2020-12-24 18:39:22 +0900

    feat: modules/projectsの実装

---
+++ client/src/redux/modules/projects.ts
@@ -0,0 +1,60 @@
+import { createSelector } from 'reselect';
+import actionCreatorFactory from 'typescript-fsa';
+import { reducerWithInitialState } from 'typescript-fsa-reducers';
+import { dummyProjects } from '../../config/dummydata';
+import { Project } from './project';
+import { RootState } from './reducers';
+
+export interface Projects {
+  list: Project[];
+}
+type CreatePayload = {
+  title: string;
+  dueDate: Date;
+  overview: string;
+  userId: string;
+};
+const actionCreator = actionCreatorFactory();
+
+// 本当は非同期処理
+const create = actionCreator<CreatePayload>('CREATE_PROJECT');
+
+const INITIAL_STATE: Projects = { list: dummyProjects };
+
+const reducer = reducerWithInitialState(INITIAL_STATE).case(
+  create,
+  (state, payload) => ({ ...state, ...payload }),
+);
+
+export default reducer;
+
+export const selectProjects = createSelector(
+  [(state: RootState) => state.resources.projects.list],
+  (projects) => projects,
+);
+
+export const selectOpenProjects = createSelector(
+  [
+    (state: RootState) => state.resources.projects.list,
+    (state: RootState) => state.resources.tasks.list,
+  ],
+  (projects, tasks) =>
+    projects.filter((project) =>
+      tasks
+        .filter((task) => task.projectId === project.id)
+        .some((task) => task.status !== 'COMPLETE'),
+    ),
+);
+
+export const selectCloseProjects = createSelector(
+  [
+    (state: RootState) => state.resources.projects.list,
+    (state: RootState) => state.resources.tasks.list,
+  ],
+  (projects, tasks) =>
+    projects.filter((project) =>
+      tasks
+        .filter((task) => task.projectId === project.id)
+        .every((task) => task.status === 'COMPLETE'),
+    ),
+);

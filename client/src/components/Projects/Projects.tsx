c74a29981bd05f1b93ccc2a07613f65bfce5ddae
Shun Nakano <nakano_syun@waku-2.com>
2020-12-24 18:40:57 +0900

    feat: reduxと接続

--- client/src/components/Projects/Projects.tsx
+++ client/src/components/Projects/Projects.tsx
@@ -1,16 +1,28 @@
 import React from 'react';
+import { Project } from '../../redux/modules/project';
+import { Task } from '../../redux/modules/task';
 import { ProjectList } from '../ProjectList';
 import styles from './index.module.scss';
 
-export const Projects: React.VFC = () => (
+type Props = {
+  openProjects: Project[];
+  closeProjects: Project[];
+  tasks: Task[];
+};
+export const Projects: React.VFC<Props> = ({
+  openProjects,
+  closeProjects,
+  tasks,
+}) => (
   <>
     <section className={styles.wrpper}>
+      {/* add bottuonの追加 */}
       <h2 className={styles.title}>Open Project List</h2>
-      <ProjectList />
+      <ProjectList projects={openProjects} tasks={tasks} />
     </section>
     <section className={styles.wrpper}>
       <h2 className={styles.title}>Close Project List</h2>
-      <ProjectList />
+      <ProjectList projects={closeProjects} tasks={tasks} />
     </section>
   </>
 );
--- client/src/components/Projects/index.tsx
+++ client/src/components/Projects/index.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { withAuth } from '../../helpers/withAuth';
+import {
+  selectOpenProjects,
+  selectCloseProjects,
+} from '../../redux/modules/projects';
+import { selectTasks } from '../../redux/modules/tasks';
 import { Projects as Presentational } from './Projects';
 
-const Component: React.VFC = () => <Presentational />;
+const Component: React.VFC = () => {
+  const openProjects = useSelector(selectOpenProjects);
+  const closeProjects = useSelector(selectCloseProjects);
+  const tasks = useSelector(selectTasks);
+
+  return (
+    <Presentational
+      openProjects={openProjects}
+      closeProjects={closeProjects}
+      tasks={tasks}
+    />
+  );
+};
 
 export const Projects = withAuth(Component);

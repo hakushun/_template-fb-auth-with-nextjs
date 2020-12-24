f9c1a69022b8ac3bf8f1f1457e54c212c029eecc
Shun Nakano <nakano_syun@waku-2.com>
2020-12-24 18:41:00 +0900

    feat: reduxと接続

--- client/src/components/Tasks/Tasks.tsx
+++ client/src/components/Tasks/Tasks.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
+import { Task } from '../../redux/modules/task';
 import { TaskList } from '../TaskList';
 import styles from './index.module.scss';
 
-export const Tasks: React.VFC = () => (
+type Props = {
+  openTasks: Task[];
+  closeTasks: Task[];
+};
+export const Tasks: React.VFC<Props> = ({ openTasks, closeTasks }) => (
   <>
     <section className={styles.wrpper}>
+      {/* add bottuonの追加 */}
       <h2 className={styles.title}>Open Task List</h2>
-      <TaskList />
+      <TaskList tasks={openTasks} />
     </section>
     <section className={styles.wrpper}>
       <h2 className={styles.title}>Close Task List</h2>
-      <TaskList />
+      <TaskList tasks={closeTasks} />
     </section>
   </>
 );
--- client/src/components/Tasks/index.tsx
+++ client/src/components/Tasks/index.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { withAuth } from '../../helpers/withAuth';
+import { selectOpenTasks, selectCloseTasks } from '../../redux/modules/tasks';
 import { Tasks as Presentational } from './Tasks';
 
-const Component: React.VFC = () => <Presentational />;
+const Component: React.VFC = () => {
+  const openTasks = useSelector(selectOpenTasks);
+  const closeTasks = useSelector(selectCloseTasks);
+
+  return <Presentational openTasks={openTasks} closeTasks={closeTasks} />;
+};
 
 export const Tasks = withAuth(Component);

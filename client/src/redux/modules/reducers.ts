b43c34749023b6a7bc33d805844ce3a3050f22c8
Shun Nakano <nakano_syun@waku-2.com>
2020-12-24 18:39:51 +0900

    feat: modules追加

--- client/src/redux/modules/reducers.ts
+++ client/src/redux/modules/reducers.ts
@@ -3,10 +3,16 @@ import user from './user';
 import dialog from './dialog';
 import dropdown from './dropdown';
 import modal from './modal';
+import project from './project';
+import projects from './projects';
+import task from './task';
+import tasks from './tasks';
+import activity from './activity';
+import activities from './activities';
 
 const rootReducer = combineReducers({
-  resources: combineReducers({ user }),
-  ui: combineReducers({ dialog, dropdown, modal }),
+  resources: combineReducers({ user, projects, tasks, activities }),
+  ui: combineReducers({ dialog, dropdown, modal, project, task, activity }),
 });
 
 export type RootState = ReturnType<typeof rootReducer>;

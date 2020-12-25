3d04e5b499f7fd2a9baf0039e078456b9ac80e4e
Shun Nakano <nakano_syun@waku-2.com>
2020-12-25 13:11:26 +0900

    refactor: AddPayloadの修正

--- client/src/redux/modules/activity.ts
+++ client/src/redux/modules/activity.ts
@@ -12,33 +12,25 @@ export interface Activity {
   createdAt?: Date;
   updatedAt?: Date;
 }
-type RelatedId = 'projectId' | 'taskId';
-// projectId | taskIdのどっちか一方が必ず含まれる
-export type AddPayload<K extends RelatedId> = {
-  // eslint-disable-next-line no-unused-vars
-  [key in K]: string;
-};
+export type AddPayload =
+  | {
+      projectId: string;
+    }
+  | {
+      taskId: string;
+    };
 const actionCreator = actionCreatorFactory();
 
-export const add = actionCreator<AddPayload<RelatedId>>('ADD_ACTIVITY');
+export const add = actionCreator<AddPayload>('ADD_ACTIVITY');
 export const edit = actionCreator<{ id: string }>('EDIT_ACTIVITY');
 
 const INITIAL_STATE: Activity = { comment: '' };
 
 const reducer = reducerWithInitialState(INITIAL_STATE)
-  .case(add, (state, payload) => {
-    if (payload.projectId)
-      return {
-        ...state,
-        projectId: payload.projectId,
-      };
-    if (payload.taskId)
-      return {
-        ...state,
-        taskId: payload.taskId,
-      };
-    return { ...INITIAL_STATE };
-  })
+  .case(add, (state, payload) => ({
+    ...INITIAL_STATE,
+    ...payload,
+  }))
   .case(edit, (state, payload) => ({
     ...state,
     id: payload.id,

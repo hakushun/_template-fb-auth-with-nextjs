bb9619b548a13d00a32ad1e75ef8e82210d5aa0c
Shun Nakano <nakano_syun@waku-2.com>
2020-12-25 17:47:34 +0900

    feat: addボタンの実装

--- client/src/components/Tasks/Tasks.tsx
+++ client/src/components/Tasks/Tasks.tsx
@@ -1,17 +1,31 @@
 import React from 'react';
 import { Task } from '../../redux/modules/task';
+import { TaskForm } from '../TaskForm';
 import { TaskList } from '../TaskList';
 import styles from './index.module.scss';
 
 type Props = {
   openTasks: Task[];
   closeTasks: Task[];
+  handleAddTask: () => void;
 };
-export const Tasks: React.VFC<Props> = ({ openTasks, closeTasks }) => (
+export const Tasks: React.VFC<Props> = ({
+  openTasks,
+  closeTasks,
+  handleAddTask,
+}) => (
   <>
+    <TaskForm />
     <section className={styles.wrpper}>
-      {/* add bottuonの追加 */}
-      <h2 className={styles.title}>Open Task List</h2>
+      <div className={styles.heading}>
+        <h2 className={styles.title}>Open Task List</h2>
+        <button
+          type="button"
+          className={styles.action}
+          onClick={() => handleAddTask()}>
+          <img src="/images/icon-circle-plus.svg" alt="タスクを追加する" />
+        </button>
+      </div>
       <TaskList tasks={openTasks} />
     </section>
     <section className={styles.wrpper}>

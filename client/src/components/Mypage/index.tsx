b5a0b99497db6a5a1a4f2bbee49860e63ba5fe45
Shun Nakano <nakano_syun@waku-2.com>
2020-12-25 13:10:51 +0900

    refactor: Mypageに渡すprojectsをOpenのみに修正

--- client/src/components/Mypage/index.tsx
+++ client/src/components/Mypage/index.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { Mypage as Presentational } from './Mypage';
 import { withAuth } from '../../helpers/withAuth';
-import { selectProjects } from '../../redux/modules/projects';
+import { selectOpenProjects } from '../../redux/modules/projects';
 import { selectOpenTasks, selectTasks } from '../../redux/modules/tasks';
 
 const Component: React.VFC = () => {
-  const projects = useSelector(selectProjects);
+  const projects = useSelector(selectOpenProjects);
   const tasks = useSelector(selectTasks);
   const openTasks = useSelector(selectOpenTasks);
 

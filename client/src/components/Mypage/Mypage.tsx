9efaa80a922c95cc6976fff007c6c1dbd7439fd3
Shun Nakano <shun.nakano@nijibox.co.jp>
2020-12-26 14:30:15 +0900

    feat: Mypageにaddボタン追加

--- client/src/components/Mypage/Mypage.tsx
+++ client/src/components/Mypage/Mypage.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Project } from '../../redux/modules/project';
 import { Task } from '../../redux/modules/task';
+import { ProjectForm } from '../ProjectForm';
 import { ProjectList } from '../ProjectList';
+import { TaskForm } from '../TaskForm';
 import { TaskList } from '../TaskList';
 import styles from './index.module.scss';
 
@@ -9,17 +11,41 @@ type Props = {
   projects: Project[];
   tasks: Task[];
   openTasks: Task[];
+  handleAddProject: () => void;
+  handleAddTask: () => void;
 };
-export const Mypage: React.VFC<Props> = ({ projects, tasks, openTasks }) => (
+export const Mypage: React.VFC<Props> = ({
+  projects,
+  tasks,
+  openTasks,
+  handleAddProject,
+  handleAddTask,
+}) => (
   <>
+    <ProjectForm />
+    <TaskForm />
     <section className={styles.wrpper}>
-      {/* add bottuonの追加 */}
-      <h2 className={styles.title}>Open Project List</h2>
+      <div className={styles.heading}>
+        <h2 className={styles.title}>Open Project List</h2>
+        <button
+          type="button"
+          className={styles.action}
+          onClick={() => handleAddProject()}>
+          <img src="/images/icon-circle-plus.svg" alt="タスクを追加する" />
+        </button>
+      </div>
       <ProjectList projects={projects} tasks={tasks} />
     </section>
     <section className={styles.wrpper}>
-      {/* add bottuonの追加 */}
-      <h2 className={styles.title}>Open Task List</h2>
+      <div className={styles.heading}>
+        <h2 className={styles.title}>Open Task List</h2>
+        <button
+          type="button"
+          className={styles.action}
+          onClick={() => handleAddTask()}>
+          <img src="/images/icon-circle-plus.svg" alt="タスクを追加する" />
+        </button>
+      </div>
       <TaskList tasks={openTasks} />
     </section>
   </>
--- client/src/components/Mypage/index.tsx
+++ client/src/components/Mypage/index.tsx
@@ -1,17 +1,33 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Mypage as Presentational } from './Mypage';
 import { withAuth } from '../../helpers/withAuth';
 import { selectOpenProjects } from '../../redux/modules/projects';
 import { selectOpenTasks, selectTasks } from '../../redux/modules/tasks';
+import { add as addProject } from '../../redux/modules/project';
+import { add as addTask } from '../../redux/modules/task';
 
 const Component: React.VFC = () => {
+  const dispatch = useDispatch();
+
   const projects = useSelector(selectOpenProjects);
   const tasks = useSelector(selectTasks);
   const openTasks = useSelector(selectOpenTasks);
 
+  const handleAddProject = () => {
+    dispatch(addProject());
+  };
+  const handleAddTask = () => {
+    dispatch(addTask(null));
+  };
   return (
-    <Presentational projects={projects} tasks={tasks} openTasks={openTasks} />
+    <Presentational
+      projects={projects}
+      tasks={tasks}
+      openTasks={openTasks}
+      handleAddProject={handleAddProject}
+      handleAddTask={handleAddTask}
+    />
   );
 };
 

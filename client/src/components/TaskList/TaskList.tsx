dbeeed74092a7117fc0249a531117f771cc9374b
Shun Nakano <shun.nakano@nijibox.co.jp>
2020-12-26 14:52:00 +0900

    feat: Lsitに表示するitemがなかった時のスタイル実装

--- client/src/components/TaskList/TaskList.tsx
+++ client/src/components/TaskList/TaskList.tsx
@@ -10,28 +10,39 @@ type Props = {
   tasks: Task[];
   handleFocus: (_taskId: string, _projectId: string) => void;
 };
-// itemがなかった時の表示追加
 export const TaskList: React.VFC<Props> = ({ tasks, handleFocus }) => (
   <div className={styles.wrapper}>
     <ListHeader context="task" />
     <ul className={styles.list}>
-      {tasks.map((task) => (
-        <li className={styles.item} key={task.id}>
-          <Link href={`/tasks/${task.id}`}>
-            <a
-              id={`tasks_${task.id}`}
-              className={styles.link}
-              onClick={() => handleFocus(task.id!, task.projectId)}
-              onKeyPress={() => handleFocus(task.id!, task.projectId)}>
-              <div className={styles.status}>{toStringStatus(task.status)}</div>
-              <div className={styles.name}>{task.title}</div>
-              <div className={styles.duedate}>
-                {getStaringDate(task.dueDate)}
-              </div>
-            </a>
-          </Link>
+      {tasks.length === 0 ? (
+        <li className={styles.item}>
+          <div className={styles.link}>
+            <div className={styles.empty}>No Items</div>
+          </div>
         </li>
-      ))}
+      ) : (
+        <>
+          {tasks.map((task) => (
+            <li className={styles.item} key={task.id}>
+              <Link href={`/tasks/${task.id}`}>
+                <a
+                  id={`tasks_${task.id}`}
+                  className={styles.link}
+                  onClick={() => handleFocus(task.id!, task.projectId)}
+                  onKeyPress={() => handleFocus(task.id!, task.projectId)}>
+                  <div className={styles.status}>
+                    {toStringStatus(task.status)}
+                  </div>
+                  <div className={styles.name}>{task.title}</div>
+                  <div className={styles.duedate}>
+                    {getStaringDate(task.dueDate)}
+                  </div>
+                </a>
+              </Link>
+            </li>
+          ))}
+        </>
+      )}
     </ul>
   </div>
 );

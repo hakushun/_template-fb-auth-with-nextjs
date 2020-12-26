58484dcecc499e1927d3c0cc7f77d1236d71f71a
Shun Nakano <shun.nakano@nijibox.co.jp>
2020-12-26 14:31:34 +0900

    refactor: getStaringTimestampのロジック修正

--- client/src/libs/date.ts
+++ client/src/libs/date.ts
@@ -11,8 +11,8 @@ export const getStaringTimestamp = (date: Date): string => {
   const years = dt.getFullYear();
   const months = ('00' + (dt.getMonth() + 1)).slice(-2);
   const dates = ('00' + dt.getDate()).slice(-2);
-  const hours = ('00' + (dt.getHours() + 1)).slice(-2);
+  const hours = ('00' + dt.getHours()).slice(-2);
   const minutes = ('00' + dt.getMinutes()).slice(-2);
-  const seconds = ('00' + (dt.getSeconds() + 1)).slice(-2);
+  const seconds = ('00' + dt.getSeconds()).slice(-2);
   return `${years}-${months}-${dates} ${hours}:${minutes}:${seconds}`;
 };

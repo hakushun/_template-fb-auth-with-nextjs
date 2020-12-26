ecae33ea7e2193ca175fd1a9a4f9cce704dc4e2d
Shun Nakano <shun.nakano@nijibox.co.jp>
2020-12-26 16:27:00 +0900

    refactor: ListHeaderの削除

--- client/src/components/ListHeader/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { ListHeader as Presentational } from './ListHeader';
-
-type Props = {
-  context: 'project' | 'task';
-};
-export const ListHeader: React.VFC<Props> = ({ context }) => (
-  <Presentational context={context} />
-);

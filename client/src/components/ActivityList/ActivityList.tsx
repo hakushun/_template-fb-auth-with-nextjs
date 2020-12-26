c97f93a908710802c312bc66fea608a6a0e62101
Shun Nakano <shun.nakano@nijibox.co.jp>
2020-12-26 14:53:12 +0900

    feat: Lsitに表示するitemがなかった時のスタイル実装

--- client/src/components/ActivityList/ActivityList.tsx
+++ client/src/components/ActivityList/ActivityList.tsx
@@ -7,33 +7,42 @@ type Props = {
   activities: Activity[];
   handleEdit: (_id: string) => void;
 };
-// itemがなかった時の表示追加
 export const ActivityList: React.VFC<Props> = ({ activities, handleEdit }) => (
   <div className={styles.wrapper}>
     <ul className={styles.list}>
-      {activities.map((activity) => (
-        <li className={styles.item} key={activity.id}>
-          <div className={styles.itemHeader}>
-            <span className={styles.date}>
-              {getStaringTimestamp(activity.updatedAt!)}
-            </span>
-            <div className={styles.actionWrapper}>
-              <button
-                type="button"
-                className={styles.action}
-                onClick={() => handleEdit(activity.id!)}>
-                <img src="/images/icon-edit.svg" alt="編集する" />
-              </button>
-              <button type="button" className={styles.action}>
-                <img src="/images/icon-trash.svg" alt="削除する" />
-              </button>
-            </div>
-          </div>
+      {activities.length === 0 ? (
+        <li className={styles.item}>
           <div className={styles.commentWrapper}>
-            <p className={styles.comment}>{activity.comment}</p>
+            <p className={styles.empty}>No Activities</p>
           </div>
         </li>
-      ))}
+      ) : (
+        <>
+          {activities.map((activity) => (
+            <li className={styles.item} key={activity.id}>
+              <div className={styles.itemHeader}>
+                <span className={styles.date}>
+                  {getStaringTimestamp(activity.updatedAt!)}
+                </span>
+                <div className={styles.actionWrapper}>
+                  <button
+                    type="button"
+                    className={styles.action}
+                    onClick={() => handleEdit(activity.id!)}>
+                    <img src="/images/icon-edit.svg" alt="編集する" />
+                  </button>
+                  <button type="button" className={styles.action}>
+                    <img src="/images/icon-trash.svg" alt="削除する" />
+                  </button>
+                </div>
+              </div>
+              <div className={styles.commentWrapper}>
+                <p className={styles.comment}>{activity.comment}</p>
+              </div>
+            </li>
+          ))}
+        </>
+      )}
     </ul>
   </div>
 );

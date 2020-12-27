c24c6750d55941c33fce67cc4948e2a3e463951f
Shun Nakano <shun.nakano@nijibox.co.jp>
2020-12-27 14:31:55 +0900

    feat: modules/activitiesにupdateActions実装

--- client/src/redux/modules/activities.ts
+++ client/src/redux/modules/activities.ts
@@ -3,7 +3,7 @@ import { createSelector } from 'reselect';
 import actionCreatorFactory from 'typescript-fsa';
 import { reducerWithInitialState } from 'typescript-fsa-reducers';
 import { dummyActivities } from '../../config/dummydata';
-import { postActivity } from '../../libs/axios';
+import { postActivity, putActivity } from '../../libs/axios';
 import { Activity } from './activity';
 import { RootState } from './reducers';
 
@@ -16,6 +16,15 @@ export type CreatePayload = {
   projectId?: string;
   taskId?: string;
   comment: string;
+};
+export type UpdatePayload = {
+  id: string;
+  projectId?: string;
+  taskId?: string;
+  comment: string;
+  userId: string;
+  createdAt: number;
+  updatedAt: number;
 };
 type Error = {
   name: string;
@@ -34,6 +43,17 @@ export const create = (body: CreatePayload): StepAction =>
     (error) => createActions.failed({ params: body, error }),
   ]);
 
+export const updateActions = actionCreator.async<
+  UpdatePayload,
+  Activity,
+  Error
+>('UPDATE_ACTIVITY');
+export const update = (body: UpdatePayload): StepAction =>
+  steps(updateActions.started(body), () => putActivity(body), [
+    ({ data }) => updateActions.done({ params: body, result: data }),
+    (error) => updateActions.failed({ params: body, error }),
+  ]);
+
 const INITIAL_STATE: Activities = { list: dummyActivities, isLoading: false };
 
 const reducer = reducerWithInitialState(INITIAL_STATE)
@@ -43,7 +63,19 @@ const reducer = reducerWithInitialState(INITIAL_STATE)
     isLoading: false,
     list: [...state.list, result],
   }))
-  .case(createActions.failed, (state) => ({ ...state, isLoading: false }));
+  .case(createActions.failed, (state) => ({ ...state, isLoading: false }))
+  .case(updateActions.started, (state) => ({ ...state, isLoading: true }))
+  .case(updateActions.done, (state, { result }) => ({
+    ...state,
+    isLoading: false,
+    list: [
+      ...state.list.map((item) => {
+        if (item.id === result.id) return result;
+        return item;
+      }),
+    ],
+  }))
+  .case(updateActions.failed, (state) => ({ ...state, isLoading: false }));
 
 export default reducer;
 

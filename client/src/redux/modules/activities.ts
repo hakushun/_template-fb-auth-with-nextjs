f5215b788d2b6d63f304aa6039e0e3db6a216457
Shun Nakano <shun.nakano@nijibox.co.jp>
2020-12-27 12:04:22 +0900

    feat: modules/activitiesにcreateActionsを実装

--- client/src/redux/modules/activities.ts
+++ client/src/redux/modules/activities.ts
@@ -1,30 +1,49 @@
+import { StepAction, steps } from 'redux-effects-steps';
 import { createSelector } from 'reselect';
 import actionCreatorFactory from 'typescript-fsa';
 import { reducerWithInitialState } from 'typescript-fsa-reducers';
 import { dummyActivities } from '../../config/dummydata';
+import { postActivity } from '../../libs/axios';
 import { Activity } from './activity';
 import { RootState } from './reducers';
 
 export interface Activities {
   list: Activity[];
+  isLoading: boolean;
 }
 // projectId | taskIdのどっちか一方は必ず存在する
-type CreatePayload = {
+export type CreatePayload = {
   projectId?: string;
   taskId?: string;
   comment: string;
 };
+type Error = {
+  name: string;
+  message: string;
+};
 const actionCreator = actionCreatorFactory();
 
-// 本当は非同期処理
-const create = actionCreator<CreatePayload>('CREATE_ACTIVITY');
+export const createActions = actionCreator.async<
+  CreatePayload,
+  Activity,
+  Error
+>('CREATE_ACTIVITY');
+export const create = (body: CreatePayload): StepAction =>
+  steps(createActions.started(body), () => postActivity(body), [
+    ({ data }) => createActions.done({ params: body, result: data }),
+    (error) => createActions.failed({ params: body, error }),
+  ]);
 
-const INITIAL_STATE: Activities = { list: dummyActivities };
+const INITIAL_STATE: Activities = { list: dummyActivities, isLoading: false };
 
-const reducer = reducerWithInitialState(INITIAL_STATE).case(
-  create,
-  (state, payload) => ({ ...state, ...payload }),
-);
+const reducer = reducerWithInitialState(INITIAL_STATE)
+  .case(createActions.started, (state) => ({ ...state, isLoading: true }))
+  .case(createActions.done, (state, { result }) => ({
+    ...state,
+    isLoading: false,
+    list: [...state.list, result],
+  }))
+  .case(createActions.failed, (state) => ({ ...state, isLoading: false }));
 
 export default reducer;
 

125e2b9bc482dbcd083fd04c3db3faa651f695ca
Shun Nakano <shun.nakano@nijibox.co.jp>
2020-12-27 12:03:24 +0900

    feat: postActivityメソッドの作成

--- client/src/libs/axios.ts
+++ client/src/libs/axios.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { CreatePayload as CreateActivity } from '../redux/modules/activities';
 import { CreatePayload as CreateProject } from '../redux/modules/projects';
 import { CreatePayload as CreateTask } from '../redux/modules/tasks';
 
@@ -14,3 +15,6 @@ export const postProject = async (data: CreateProject): Promise<void> =>
 
 export const postTask = async (data: CreateTask): Promise<void> =>
   instance.post('/api/tasks', data);
+
+export const postActivity = async (data: CreateActivity): Promise<void> =>
+  instance.post('/api/activities', data);

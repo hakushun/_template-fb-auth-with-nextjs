375e8cf4a0392c273f9353e7f3d46ca643e6b88b
Shun Nakano <shun.nakano@nijibox.co.jp>
2020-12-27 16:41:44 +0900

    feat: axiosにdeleteメソッド実装

--- client/src/libs/axios.ts
+++ client/src/libs/axios.ts
@@ -1,14 +1,17 @@
 import axios from 'axios';
 import {
   CreatePayload as CreateActivity,
+  RemovePayload as RemoveActivity,
   UpdatePayload as UpdateActivity,
 } from '../redux/modules/activities';
 import {
   CreatePayload as CreateProject,
+  RemovePayload as RemoveProject,
   UpdatePayload as UpdateProject,
 } from '../redux/modules/projects';
 import {
   CreatePayload as CreateTask,
+  RemovePayload as RemoveTask,
   UpdatePayload as UpdateTask,
 } from '../redux/modules/tasks';
 
@@ -36,3 +39,12 @@ export const putTask = async (data: UpdateTask): Promise<void> =>
 
 export const putActivity = async (data: UpdateActivity): Promise<void> =>
   instance.put('/api/activities', data);
+
+export const deleteProject = async ({ id }: RemoveProject): Promise<void> =>
+  instance.delete(`/api/projects/${id}`);
+
+export const deleteTask = async ({ id }: RemoveTask): Promise<void> =>
+  instance.delete(`/api/tasks/${id}`);
+
+export const deleteActivity = async ({ id }: RemoveActivity): Promise<void> =>
+  instance.delete(`/api/activities/${id}`);

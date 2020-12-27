bf6dab453af9ef78faf50cc6451edb0ea54cca8b
Shun Nakano <shun.nakano@nijibox.co.jp>
2020-12-27 11:39:48 +0900

    feat: createActionsの作成

--- client/src/redux/modules/projects.ts
+++ client/src/redux/modules/projects.ts
@@ -1,7 +1,9 @@
+import { StepAction, steps } from 'redux-effects-steps';
 import { createSelector } from 'reselect';
 import actionCreatorFactory from 'typescript-fsa';
 import { reducerWithInitialState } from 'typescript-fsa-reducers';
 import { dummyProjects } from '../../config/dummydata';
+import { postProject } from '../../libs/axios';
 import { Project } from './project';
 import { RootState } from './reducers';
 import { sortProjectArray } from './sort';
@@ -9,24 +11,38 @@ import { getRelatedTasks } from './tasks';
 
 export interface Projects {
   list: Project[];
+  isLoading: boolean;
 }
-type CreatePayload = {
+export type CreatePayload = {
   title: string;
   dueDate: Date;
   detail: string;
-  userId: string;
+};
+type Error = {
+  name: string;
+  message: string;
 };
 const actionCreator = actionCreatorFactory();
 
-// 本当は非同期処理
-const create = actionCreator<CreatePayload>('CREATE_PROJECT');
-
-const INITIAL_STATE: Projects = { list: dummyProjects };
-
-const reducer = reducerWithInitialState(INITIAL_STATE).case(
-  create,
-  (state, payload) => ({ ...state, ...payload }),
+export const createActions = actionCreator.async<CreatePayload, Project, Error>(
+  'CREATE_PROJECT',
 );
+export const create = (body: CreatePayload): StepAction =>
+  steps(createActions.started(body), () => postProject(body), [
+    ({ data }) => createActions.done({ params: body, result: data }),
+    (error) => createActions.failed({ params: body, error }),
+  ]);
+
+const INITIAL_STATE: Projects = { list: dummyProjects, isLoading: false };
+
+const reducer = reducerWithInitialState(INITIAL_STATE)
+  .case(createActions.started, (state) => ({ ...state, isLoading: true }))
+  .case(createActions.done, (state, { result }) => ({
+    ...state,
+    isLoading: false,
+    list: [...state.list, result],
+  }))
+  .case(createActions.failed, (state) => ({ ...state, isLoading: false }));
 
 export default reducer;
 

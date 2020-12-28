362a8b2c7ad7e6adb8230aa5be58a0c381db8f59
Shun Nakano <shun.nakano@nijibox.co.jp>
2020-12-28 11:24:06 +0900

    feat: axiosにuser関連の関数追加

--- client/src/libs/axios.ts
+++ client/src/libs/axios.ts
@@ -14,6 +14,10 @@ import {
   RemovePayload as RemoveTask,
   UpdatePayload as UpdateTask,
 } from '../redux/modules/tasks';
+import {
+  RemovePayload as RemoveUser,
+  UserPayload,
+} from '../redux/modules/users';
 
 const instance = axios.create({
   headers: {
@@ -31,6 +35,9 @@ export const postTask = async (data: CreateTask): Promise<void> =>
 export const postActivity = async (data: CreateActivity): Promise<void> =>
   instance.post('/api/activities', data);
 
+export const postUser = async (data: UserPayload): Promise<void> =>
+  instance.post('/api/users', data);
+
 export const putProject = async (data: UpdateProject): Promise<void> =>
   instance.put('/api/projects', data);
 
@@ -40,6 +47,9 @@ export const putTask = async (data: UpdateTask): Promise<void> =>
 export const putActivity = async (data: UpdateActivity): Promise<void> =>
   instance.put('/api/activities', data);
 
+export const putUser = async (data: UserPayload): Promise<void> =>
+  instance.put('/api/users', data);
+
 export const deleteProject = async ({ id }: RemoveProject): Promise<void> =>
   instance.delete(`/api/projects/${id}`);
 
@@ -48,3 +58,6 @@ export const deleteTask = async ({ id }: RemoveTask): Promise<void> =>
 
 export const deleteActivity = async ({ id }: RemoveActivity): Promise<void> =>
   instance.delete(`/api/activities/${id}`);
+
+export const deleteUser = async ({ id }: RemoveUser): Promise<void> =>
+  instance.delete(`/api/users/${id}`);

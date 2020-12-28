7afec002fdc6ec6fbf23229cf7cce71842208ee1
Shun Nakano <shun.nakano@nijibox.co.jp>
2020-12-28 14:10:57 +0900

    feat: DeleteFormの接続

--- client/src/components/Profile/Profile.tsx
+++ client/src/components/Profile/Profile.tsx
@@ -3,66 +3,91 @@ import React from 'react';
 import { Field, Form } from 'react-final-form';
 import { composeValidators, isRequired } from '../../libs/validations';
 import { User } from '../../redux/modules/user';
+import { DeleteForm } from '../DeleteForm';
 import styles from './index.module.scss';
 
 type Props = {
   initialValues: User;
   handleUpdate: (_value: User) => void;
+  openModal: () => void;
 };
-export const Profile: React.VFC<Props> = ({ initialValues, handleUpdate }) => (
-  <section className={styles.profile}>
-    <Form
-      onSubmit={handleUpdate}
-      subscription={{ submitting: true }}
-      initialValues={initialValues}
-      render={({ handleSubmit }) => (
-        <form onSubmit={handleSubmit} className={styles.form}>
-          <fieldset>
-            <legend>
-              <h2 className={styles.title}>Profile</h2>
-            </legend>
-            <Field
-              name="username"
-              validate={composeValidators(isRequired)}
-              subscription={{
-                value: true,
-                active: true,
-                error: true,
-                touched: true,
-              }}>
-              {({ input, meta }) => (
-                <div className={styles.inputWrapper}>
-                  <div className={styles.labelWrapper}>
-                    <label htmlFor="profile_username" className={styles.label}>
-                      User name
-                    </label>
+export const Profile: React.VFC<Props> = ({
+  initialValues,
+  handleUpdate,
+  openModal,
+}) => (
+  <>
+    <DeleteForm />
+    <section className={styles.profile}>
+      <Form
+        onSubmit={handleUpdate}
+        subscription={{ submitting: true }}
+        initialValues={initialValues}
+        render={({ handleSubmit }) => (
+          <form onSubmit={handleSubmit} className={styles.form}>
+            <fieldset>
+              <legend>
+                <h2 className={styles.title}>Profile</h2>
+              </legend>
+              <Field
+                name="username"
+                validate={composeValidators(isRequired)}
+                subscription={{
+                  value: true,
+                  active: true,
+                  error: true,
+                  touched: true,
+                }}>
+                {({ input, meta }) => (
+                  <div className={styles.inputWrapper}>
+                    <div className={styles.labelWrapper}>
+                      <label
+                        htmlFor="profile_username"
+                        className={styles.label}>
+                        User name
+                      </label>
+                    </div>
+                    <input
+                      id="profile_username"
+                      type="text"
+                      placeholder="User name"
+                      // disabled={isLoading}
+                      maxLength={50}
+                      className={clsx(
+                        styles.input,
+                        meta.touched && meta.error && styles.hasError,
+                      )}
+                      {...input}
+                    />
                   </div>
-                  <input
-                    id="profile_username"
-                    type="text"
-                    placeholder="User name"
-                    // disabled={isLoading}
-                    maxLength={50}
-                    className={clsx(
-                      styles.input,
-                      meta.touched && meta.error && styles.hasError,
-                    )}
-                    {...input}
-                  />
-                </div>
-              )}
-            </Field>
-            <div className={styles.actionWrapper}>
-              <button
-                type="submit"
-                // disabled={isLoading}
-                className={styles.action}>
-                Update
-              </button>
-            </div>
-          </fieldset>
-        </form>
-      )}
-    />
-  </section>
+                )}
+              </Field>
+              <div className={styles.actionWrapper}>
+                <button
+                  type="submit"
+                  // disabled={isLoading}
+                  className={styles.action}>
+                  Update
+                </button>
+              </div>
+            </fieldset>
+          </form>
+        )}
+      />
+      <div>
+        <button
+          type="button"
+          className={styles.delete}
+          onClick={() => openModal()}>
+          Delete
+          <img
+            src="/images/icon-trash.svg"
+            alt="アカウントを削除する"
+            width="20"
+            height="20"
+          />
+        </button>
+      </div>
+    </section>
+  </>
 );
--- client/src/components/Profile/index.tsx
+++ client/src/components/Profile/index.tsx
@@ -1,18 +1,30 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { withAuth } from '../../helpers/withAuth';
 import { useUser } from '../../libs/auth/useUser';
+import { toggleDeleteForm } from '../../redux/modules/modal';
 import { selectUser, User } from '../../redux/modules/user';
 import { Profile as Presentational } from './Profile';
 
 const Component: React.VFC = () => {
+  const dispatch = useDispatch();
   const user = useSelector(selectUser);
   const { updateUsername } = useUser();
 
   const handleUpdate = (value: User) => {
     updateUsername(value.username);
   };
-  return <Presentational initialValues={user} handleUpdate={handleUpdate} />;
+  const openModal = () => {
+    dispatch(toggleDeleteForm(true));
+  };
+
+  return (
+    <Presentational
+      initialValues={user}
+      handleUpdate={handleUpdate}
+      openModal={openModal}
+    />
+  );
 };
 
 export const Profile = withAuth(Component);

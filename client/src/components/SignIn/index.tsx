c920331185fd54f4161d490196489df5c9cb21ef
Shun Nakano <shun.nakano@nijibox.co.jp>
2020-12-28 14:10:38 +0900

    feat: PasswordResetFormの接続

--- client/src/components/SignIn/index.tsx
+++ client/src/components/SignIn/index.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useAuth } from '../../libs/auth/useAuth';
 import { selectDialog, selectDialogMessage } from '../../redux/modules/dialog';
+import { toggleResetPasswordForm } from '../../redux/modules/modal';
 import { SignIn as Presentational } from './SignIn';
 
 export const SignIn: React.VFC = () => {
+  const dispatch = useDispatch();
   const dialogIsOpened = useSelector(selectDialog);
   const dialogMessage = useSelector(selectDialogMessage);
   const { signin } = useAuth();
+
+  const openResetPasswordForm = () => {
+    dispatch(toggleResetPasswordForm(true));
+  };
   return (
     <Presentational
       isOpend={dialogIsOpened}
       message={dialogMessage}
       signin={signin}
+      openResetPasswordForm={openResetPasswordForm}
     />
   );
 };
